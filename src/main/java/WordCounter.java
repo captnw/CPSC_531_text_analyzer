b7eb0d92e28e9a744c81443945d9640fd0c0fb96
William Nguyen <williamngoc93@hotmail.com>
2024-04-21 19:14:51 -0700

    Updated wordcount mapreduce job

--- src/main/java/WordCounter.java
+++ src/main/java/WordCounter.java
@@ -1,5 +1,6 @@
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -9,52 +10,66 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class WordCounter {
-    public static class JoinMapMostCommonWords extends Mapper<LongWritable, Text, Text, Text> {
+    public static class WordTokenMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+        private final boolean allLowercase = true;
+
+        private final IntWritable one = new IntWritable(1); // stores the one value
+
+        private final Set<String> ignorePatterns = new HashSet<String>();
+
+        @Override
+        public void setup(Context context) throws IOException {
+            Configuration conf = context.getConfiguration();
+
+            // hardcoded for now
+            ignorePatterns.add("\\.");
+            ignorePatterns.add("\\,");
+            ignorePatterns.add("!");
+        }
+
         @Override
         protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-            // super.map(key, value, context);
-            StringTokenizer itr = new StringTokenizer(value.toString());
-            String first_word = itr.nextToken();
-            System.out.println("CWID : " + first_word + " and key" + key + " and value: " + value);
-            context.write(new Text(first_word), value);
-        }
-    }
+            // Preprocess the sentence
+            String line;
+            if (allLowercase){
+                line = value.toString().toLowerCase();
+            } else {
+                line = value.toString();
+            }
 
-    public static class JoinReduceMostCommonWords extends Reducer<Text, Text, Text, Text> {
-        @Override
-        protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-            // Hashmap ... iterate over a sentence, for each word, if ti doesn't exist
-            // in hashmap, initialize key value where value is 0 and key is word
-            // otherwise increment value in hashmap
-            HashMap<String, Integer> wordCount = new HashMap<String, Integer>();
-
-            for (Text sentence : values) {
-                StringTokenizer tokenizer = new StringTokenizer(sentence.toString());
-                while (tokenizer.hasMoreTokens()) {
-                    String word = tokenizer.nextToken();
-                    Integer count = wordCount.get(word);
-                    if (count == null) {
-                        wordCount.put(word, 1);
-                    } else {
-                        wordCount.put(word, ++count);
-                    }
+            for (String pattern : ignorePatterns) {
+                String oldline = line;
+                line = line.replaceAll(pattern, "");
+                if (!oldline.equals(line)) {
+                    System.out.println(oldline + " new: " + line);
                 }
             }
 
-            // Iterate through hashmap and write out the values
-            // word value
-//        wordCount.forEach(
-//                (key2, value) -> System.out.println(key2 + " " + value)
-//        );
+            // Tokenize the sentence
+            StringTokenizer itr = new StringTokenizer(line);
 
-            for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
-                context.write(new Text(entry.getKey()), new Text(entry.getValue() + ""));
+            // Iterate through all tokens and create a new Text, IntWritable tuple2
+            while (itr.hasMoreTokens()) {
+                Text current_word = new Text(itr.nextToken());
+                // currently, there is a count of one for this word
+                // this will tie in the reduce task
+                //System.out.println(current_word);
+                context.write(current_word, one);
             }
+        }
+    }
+
+    public static class SumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+        @Override
+        protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            context.write(key, new IntWritable(sum));
         }
     }
 
@@ -65,17 +80,15 @@ public class WordCounter {
 
         job.setJarByClass(WordCounter.class);
 
-        job.setMapperClass(JoinMapMostCommonWords.class);
-        job.setReducerClass(JoinReduceMostCommonWords.class);
+        job.setMapperClass(WordTokenMapper.class);
+        job.setCombinerClass(SumReducer.class);
+        job.setReducerClass(SumReducer.class);
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(Text.class);
-
-        //job.setInputFormatClass(TextInputFormat.class);
-        //job.setOutputFormatClass(TextOutputFormat.class);
+        job.setOutputValueClass(IntWritable.class);
 
         // Delete output2 folder if it already exists on local file system
         // Remove if ready
-        LocalFileHelperAPI.DeleteDirectory(args[1]);
+        // LocalFileHelperAPI.DeleteDirectory(args[1]);
 
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
